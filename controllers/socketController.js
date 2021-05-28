edb65e86b2a4c2ae2506c0de1d9619452649bc00
Mohammad Dehghanpour <m.dehghanpour10@gmail.com>
2021-05-28 14:47:12 +0430

    comment redis code for deploy to heroku & add online person to mongodb

--- controllers/socketController.js
+++ controllers/socketController.js
@@ -2,8 +2,8 @@ const Community = require('../models/community');
 const User = require('../models/user.js');
 const Message = require('../models/message');
 const moment = require('moment');
-const Redis = require('ioredis');
-const redis = new Redis();
+// const Redis = require('ioredis');
+// const redis = new Redis();
 sendMessage = async (io, socket, newMessage) => {
     const user = await User.findById(socket.userId);
     const community = await Community.findOne({name: socket.room});
@@ -23,18 +23,18 @@ sendMessage = async (io, socket, newMessage) => {
 };
 fetchOnlinePerson = async (socket) => {
     const onlineNames = [];
-    await redis.llen('onlineList:' + socket.room).then(async len => {
-        for (let i = 0; i < len + 1; i++) {
-            await redis.lindex('onlineList:' + socket.room, i).then(id => {
-                redis.get(id).then(name => {
-                    if (name) {
-                        onlineNames.push({name: name, _id: id})
-                    }
-                })
-            })
-        }
-
-    });
+    // await redis.llen('onlineList:' + socket.room).then(async len => {
+    //     for (let i = 0; i < len + 1; i++) {
+    //         await redis.lindex('onlineList:' + socket.room, i).then(id => {
+    //             redis.get(id).then(name => {
+    //                 if (name) {
+    //                     onlineNames.push({name: name, _id: id})
+    //                 }
+    //             })
+    //         })
+    //     }
+    //
+    // });
     return onlineNames;
 };
 exports.allocateRoom = async (io, socket, {room, userId, name}) => {
@@ -42,40 +42,40 @@ exports.allocateRoom = async (io, socket, {room, userId, name}) => {
     socket.room = room;
     socket.userId = userId;
     socket.join(room);
-    redis.get(userId).then(async r => {
+    // redis.get(userId).then(async r => {
+    //
+    //     if (!r) {
+    await sendMessage(io, socket, 'join in community !!');
+    //         redis.set(userId, name).then(r => {
+    //         });
+    //         redis.rpush('onlineList:' + socket.room, userId).then(r => {
+    //         });
+    //     }
+    const onlineNames = await fetchOnlinePerson(socket);
 
-        if (!r) {
-            await sendMessage(io, socket, 'join in community !!');
-            redis.set(userId, name).then(r => {
-            });
-            redis.rpush('onlineList:' + socket.room, userId).then(r => {
-            });
-        }
-        const onlineNames = await fetchOnlinePerson(socket);
-
-        io.to(socket.room).emit('online', onlineNames);
-
-    });
+    io.to(socket.room).emit('online', onlineNames);
+    //
+    // });
 };
 disconnect = async (io, socket) => {
 
     await sendMessage(io, socket, 'has left the community');
-    await redis.llen('onlineList:' + socket.room).then(async len => {
-        for (let i = 0; i < len + 1; i++) {
-
-            await redis.rpop('onlineList:' + socket.room).then(async id => {
-
-                if (id !== socket.userId) {
-                    await redis.lpush('onlineList:' + socket.room, id).then(() => {
-                    });
-                } else {
-                    await redis.del(id);
-
-
-                }
-            })
-        }
-    });
+    // await redis.llen('onlineList:' + socket.room).then(async len => {
+    //     for (let i = 0; i < len + 1; i++) {
+    //
+    //         await redis.rpop('onlineList:' + socket.room).then(async id => {
+    //
+    //             if (id !== socket.userId) {
+    //                 await redis.lpush('onlineList:' + socket.room, id).then(() => {
+    //                 });
+    //             } else {
+    //                 await redis.del(id);
+    //
+    //
+    //             }
+    //         })
+    //     }
+    // });
     const onlineNames = await fetchOnlinePerson(socket);
     io.to(socket.room).emit('online', onlineNames);
 
